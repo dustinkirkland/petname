399a6a8b966791f3b89b332887d5c439c244cbba
Dustin Kirkland <dustin.kirkland@gmail.com>
2014-12-16 13:07:20 -0600

    * debian/copyright, debian/petname.install => debian/install,   usr/bin/petname, usr/lib/petname/petname.sh:   - install README, which is used by downstream packages   - move functions to common library   - use common library in shell script

---
+++ usr/lib/petname/petname.sh
@@ -0,0 +1,29 @@
+# petname
+#
+# Copyright 2014 Dustin Kirkland <dustin.kirkland@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PKG="petname"
+
+RandAdverb() {
+	_RET=$(shuf -n 1 /usr/lib/$PKG/adverbs.txt)
+}
+
+RandAdjective() {
+	_RET=$(shuf -n 1 /usr/lib/$PKG/adjectives.txt)
+}
+
+RandName() {
+	_RET=$(shuf -n 1 /usr/lib/$PKG/names.txt)
+}
